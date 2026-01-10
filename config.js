4906f0479c76d25fd57be145ced35137adcc7bb2
GADV-mano-a-mano <gadv.mano.a.mano@gmail.com>
2026-01-09 21:50:30 -0400

    Clean up Firebase configuration in config.js
    Removed unused Firebase import and initialization.

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // config.js
 export const CONFIG = {
   // ===== Firebase Web App config (ya lo tienes) =====
-  import { initializeApp } from "firebase/app";
   firebaseConfig: {
     apiKey: "AIzaSyA3f-IdR6O_kl3LLeru3vNG1qFgBGJg_O0",
     authDomain: "info-board-v3.firebaseapp.com",
@@ -10,7 +9,6 @@ export const CONFIG = {
     messagingSenderId: "70958093532",
     appId: "1:70958093532:web:750f05dc1606da3d789019",
   },
-  const app = initializeApp(firebaseConfig);
 
   // ===== Admins (los mismos de tus rules) =====
   adminEmails: [
